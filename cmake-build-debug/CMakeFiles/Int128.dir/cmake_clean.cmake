f25e31ab9e3fc7ecbeb26d1ea7adb8f47e6ea9d7
cpsusie <cpsusie@hotmail.com>
2020-12-13 15:23:04 -0500

    Skeleton c++ 20 project

---
+++ cmake-build-debug/CMakeFiles/Int128.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Int128.dir/cpp/src/cpp/program.cpp.o"
+  "Int128"
+  "Int128.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Int128.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/src/cpp/program.cpp
@@ -0,0 +1,40 @@
+#include <concepts>
+#include <iostream>
+#include <iomanip>
+#include <string_view>
+#include <limits>
+#include <cstdint>
+namespace cjm
+{
+    constexpr auto newl = '\n';
+    template<typename T>
+    concept unsigned_integer = std::numeric_limits<T>::is_integer && !std::numeric_limits<T>::is_signed;
+
+    template<unsigned_integer TUInt>
+    void print_spec(TUInt val)
+    {
+        std::cout << std::hex << "[0x" << val << "]." << newl;
+    }
+
+    template<typename T>
+    void print_spec(T val)
+    {
+        std::cout << "Thing that isn't unsigned integer: [" << std::dec << val << "]." << newl;
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    using namespace std::string_view_literals;
+    std::uint64_t ui = 0xc0de'd00d'fea2'b00bu;
+    std::int64_t  i1 = 938'336;
+    constexpr auto text = "Hi mom!"sv;
+
+    cjm::print_spec(ui);
+    cjm::print_spec(i1);
+    cjm::print_spec(text);
+
+    std::cout << "Bye now!" << std::endl;
+}
+
