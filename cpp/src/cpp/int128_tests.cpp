6bf826d8b01568dd85ca4bbf2a1a088188267638
Christopher Susie <cpsusie@hotmail.com>
2020-12-27 19:17:38 -0500

    Start expanding unit test capabilities.

--- cpp/src/cpp/int128_tests.cpp
+++ cpp/src/cpp/int128_tests.cpp
@@ -1,5 +1,10 @@
 #include "int128_tests.hpp"
 
+namespace
+{
+    using uint128_t = cjm::numerics::uint128;
+    using uint128_ctrl = boost::multiprecision::uint128_t;
+}
 
 cjm::uint128_tests::ctrl_uint128_t cjm::uint128_tests::to_ctrl(uint128_t convert) 
 {
@@ -212,6 +217,93 @@ void cjm::uint128_tests::test_interconversion(const ctrl_uint128_t& control, uin
     uint128_t ctrl_to_test = to_test(control);
     ctrl_uint128_t test_to_control = to_ctrl(test);
     cjm::testing::cjm_assert(test_to_control == control && ctrl_to_test == test);
+}
+
+cjm::uint128_tests::binary_operation::binary_operation() noexcept : m_op{cjm::uint128_tests::binary_op::left_shift }, m_lhs{}, m_rhs{} {}
+
+cjm::uint128_tests::binary_operation::binary_operation(binary_op op, uint128_t first_operand, uint128_t second_operand,
+    bool calculate_now) : m_op{ op }, m_lhs{ first_operand }, m_rhs{ second_operand }, m_result{}
+{
+    size_t op_code = static_cast<size_t>(op);
+    if (op_code >= op_name_lookup.size())
+        throw std::invalid_argument{ "The op code is not recognized." };
+    if (calculate_now)
+    {
+        this->calculate_result();
+    }
+}
+
+cjm::uint128_tests::binary_operation::
+binary_operation(binary_op op, uint128_t first_operand, uint128_t second_operand)
+	: binary_operation(op, first_operand, second_operand, false){}
+
+
+cjm::uint128_tests::binary_operation::
+binary_operation(binary_op op, uint128_t first_operand, uint128_t second_operand, uint128_t result) :
+    m_op{ op }, m_lhs{ first_operand }, m_rhs{ second_operand }, m_result{ result }
+{
+    size_t op_code = static_cast<size_t>(op);
+    if (op_code >= op_name_lookup.size())
+        throw std::invalid_argument{ "The op code is not recognized." };
+}
+
+bool cjm::uint128_tests::binary_operation::do_calculate_result()
+{
+    uint128_t result = perform_calculate_result(m_lhs, m_rhs, m_op);
+    const bool changed_value = (!m_result.has_value() || *m_result != result);
+    m_result = result;
+    return changed_value;
+}
+
+uint128_t cjm::uint128_tests::binary_operation::perform_calculate_result(uint128_t lhs, uint128_t rhs, binary_op op) noexcept
+{
+    assert(static_cast<size_t>(op) < op_name_lookup.size());
+    uint128_t ret = 0;
+    switch (op)
+    {
+    case binary_op::left_shift:
+        ret = lhs << static_cast<int>(rhs);
+        break;
+    case binary_op::right_shift:
+        ret = lhs >> static_cast<int>(rhs);
+        break;
+    case binary_op::bw_and:
+        ret = lhs & rhs;
+        break;
+    case binary_op::bw_or:
+        ret = lhs | rhs;
+        break;
+    case binary_op::bw_xor:
+        ret = lhs ^ rhs;
+        break;
+    case binary_op::divide:
+        ret = lhs / rhs;
+        break;
+    case binary_op::modulus:
+        ret = lhs % rhs;
+        break;
+    case binary_op::add:
+        ret = lhs + rhs;
+        break;
+    case binary_op::subtract:
+        ret = lhs - rhs;
+        break;
+    case binary_op::multiply:
+        ret = lhs * rhs;
+        break;
+    case binary_op::compare:
+        if (lhs == rhs)
+        {
+            ret = 0;
+        }
+        else
+        {
+            ret = lhs > rhs ? 1 : -1;
+        }
+        break;
+
+    }
+    return ret;
 }
 
 
--- cpp/src/headers/int128_tests.hpp
+++ cpp/src/headers/int128_tests.hpp
@@ -30,7 +30,36 @@ namespace cjm::uint128_tests
     using std::cout;
     constexpr auto newl = '\n';
     using cout_saver = boost::io::ios_flags_saver;
+    using sv_t = std::string_view;
+    constexpr size_t binary_op_count = 11;
+    enum class binary_op : unsigned int
+    {
+        left_shift = 0,
+        right_shift,
+        bw_and,
+        bw_or,
+        bw_xor,
 
+        divide,
+        modulus,
+        add,
+        subtract,
+        multiply,
+
+        compare
+    };
+    constexpr std::array<sv_t, binary_op_count> op_name_lookup =
+        std::array<sv_t, binary_op_count>{
+        "LeftShift"sv, "RightShift"sv,
+            "And"sv, "Or"sv,
+            "Xor"sv, "Divide"sv,
+            "Modulus"sv, "Add"sv,
+            "Subtract"sv, "Multiply"sv,
+            "Compare"sv};
+
+    struct binary_operation;
+    class cjm_helper_rgen;
+	
     template<typename Invocable>
     void execute_test(Invocable test, std::string_view test_name);
 
@@ -71,7 +100,80 @@ namespace cjm::uint128_tests
 
     void test_interconversion(const ctrl_uint128_t& control, uint128_t test);
 
-	
+    struct binary_operation final
+    {
+        using uint128_t = cjm::numerics::uint128;
+        friend std::size_t hash_value(const binary_operation& obj)
+        {
+
+            std::size_t seed = 0x1FBB0493;
+            boost::hash_combine(seed, static_cast<size_t>(obj.m_op));
+            boost::hash_combine(seed, std::hash<uint128_t>{}(obj.m_lhs));
+            boost::hash_combine(seed, std::hash<uint128_t>{}(obj.m_rhs));
+            return seed;
+        }
+
+        friend bool operator<(const binary_operation& lhs, const binary_operation& rhs)
+        {
+            if (lhs.m_op < rhs.m_op)
+                return true;
+            if (rhs.m_op < lhs.m_op)
+                return false;
+            if (lhs.m_rhs < rhs.m_rhs)
+                return true;
+            if (rhs.m_rhs < lhs.m_rhs)
+                return false;
+            return lhs.m_lhs < rhs.m_lhs;
+        }
+
+        friend bool operator<=(const binary_operation& lhs, const binary_operation& rhs) { return !(rhs < lhs); }
+
+        friend bool operator>(const binary_operation& lhs, const binary_operation& rhs) { return rhs < lhs; }
+
+        friend bool operator>=(const binary_operation& lhs, const binary_operation& rhs) { return !(lhs < rhs); }
+
+        friend bool operator==(const binary_operation& lhs, const binary_operation& rhs)
+        {
+            return lhs.m_op == rhs.m_op
+                && lhs.m_rhs == rhs.m_rhs
+                && lhs.m_lhs == rhs.m_lhs;
+        }
+
+        friend bool operator!=(const binary_operation& lhs, const binary_operation& rhs) { return !(lhs == rhs); }
+
+        [[nodiscard]] binary_op op_code() const noexcept { return m_op; }
+        [[nodiscard]] uint128_t left_operand() const noexcept { return m_lhs; }
+        [[nodiscard]] uint128_t right_operand() const noexcept { return m_rhs; }
+        [[nodiscard]] std::optional<uint128_t> result() const noexcept { return m_result; }
+        [[nodiscard]] bool has_result() const noexcept { return m_result.has_value(); }
+        [[nodiscard]] bool has_correct_result() const
+        {
+            return m_result.has_value() && binary_operation::perform_calculate_result(m_lhs, m_rhs, m_op) == m_result.value();
+        }
+
+        binary_operation() noexcept;
+        binary_operation(binary_op op, uint128_t first_operand, uint128_t second_operand, bool calculate_now);
+        binary_operation(binary_op op, uint128_t first_operand, uint128_t second_operand);
+        binary_operation(binary_op op, uint128_t first_operand, uint128_t second_operand, uint128_t result);
+        binary_operation(const binary_operation& other) noexcept = default;
+        binary_operation(binary_operation&& other) noexcept = default;
+        binary_operation& operator=(const binary_operation& other) noexcept = default;
+        binary_operation& operator=(binary_operation&& other) noexcept = default;
+        ~binary_operation() = default;
+
+        void calculate_result() { do_calculate_result(); }
+
+    private:
+
+        bool do_calculate_result();
+        static uint128_t perform_calculate_result(uint128_t lhs, uint128_t rhs, binary_op op) noexcept;
+
+        binary_op m_op;
+        uint128_t m_lhs;
+        uint128_t m_rhs;
+        std::optional<uint128_t> m_result;
+
+    };
 }
 
 template<typename Invocable>
