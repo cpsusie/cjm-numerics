db40d93b180ebcf084c3e0b6cbd619cc1775fc75
cpsusie <@tticusB1rd>
2021-02-14 22:17:20 -0500

    Update CMakeLists.txt to build example code application in Linux.  Comment out unused wnewl because clang got mad.  Added notes at top of example code application.

--- example_code/src/uint128_example/program.cpp
+++ example_code/src/uint128_example/program.cpp
@@ -13,6 +13,28 @@
 #include <optional>
 #include <exception>
 #include <stdexcept>
+//The purpose of this EXAMPLE_CODE is to demonstrate the functionality of the CJM uint128 type,
+//show how it works, and talk somewhat about its strategies on various systems. It does NOT attempt
+//to prove correctness.  Proof of correctness is available through the uint128_test_app
+//which contains extensive unit tests.  THIS application, unlike uint128_test_app
+//has NO dependencies other than a C++20 compiler that conforms to a subset of the C++20
+//standard a C++20 standard library implementation that does the same.  Please
+// consult this project's repository to learn what subset that is.  (Briefly, on Windows (tested
+// 32 and 64 bit Microsoft 19.28+, on Linux - GCC 10.2.0+ and Linux Clang 11.0.0-2+ are known to work in
+// x64 system.  x86 has not been tested.)  Hopefully, the dependency free nature of this header-only library
+// and example code application will be convenient for learning about the library.
+
+//By contrast, the UNIT TEST APPLICATION carries with it dependencies including:
+	//boost (https://github.com/boostorg)
+	//abseil[cxx17] (https://github.com/abseil/abseil-cpp)
+	//date (https://github.com/HowardHinnant/date)
+//All of the foregoing are available as vcpkg ports and were easily integrated for use in unit tests as follows:
+	//x64-windows-static (visual studio, microsoft compiler, 64-bit Intel/Amd (x86-64) static linking of dependencies, including CRT)
+	//x86-windows-static (visual studio, microsoft compiler, 32-bit Intel/Amd (x86), static linking of dependencies, including CRT)
+	//x64-linux (g++ 10.2.0 and clang 11.0.0.2 using libstdc++ on Kubuntu 20.10, default linking, using CLion as development environment)
+
+//I did not want to require you to have to build anything beyond what is required by the library for the example / demonstration code
+//because the library itself makes no use of such.
 
 namespace cjm::uint128::example_code
 {
@@ -22,7 +44,7 @@ namespace cjm::uint128::example_code
 	using namespace std::string_view_literals;
 	using std::cout;
 	constexpr auto newl = '\n';
-	constexpr auto wnewl = L'\n';
+	//constexpr auto wnewl = L'\n';
 
 	void demonstrate_addition();
 	void demonstrate_constexpr_addition();
